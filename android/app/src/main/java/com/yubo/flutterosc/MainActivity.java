5d8cff8fbe2f99778ba937317d2636c0a5d489c9
sincosab <1132225715@qq.com>
2020-11-26 19:56:25 +0800

    。。

--- android/app/src/main/java/com/yubo/flutterosc/MainActivity.java
+++ android/app/src/main/java/com/yubo/flutterosc/MainActivity.java
@@ -1,18 +1,6 @@
 package com.yubo.flutterosc;
 
-import android.os.Build;
-import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      getWindow().setStatusBarColor(0);
-    }
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
